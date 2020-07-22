c27e9ab67e187f1c6e0fdfde3d30c75391a7a93a
sanchakshkaul <sanchakshkaul@gmail.com>
2020-07-22 22:33:48 +0530

    Complete with 1 issue in ChatActivity.kt

--- app/src/main/java/com/example/chatall/ChatActivity.kt
+++ app/src/main/java/com/example/chatall/ChatActivity.kt
@@ -20,35 +20,28 @@ class ChatActivity : AppCompatActivity() {
         button2.setOnClickListener {
             val msg = editTextTextPersonName2.text.toString()
             val email = FirebaseAuth.getInstance().currentUser!!.email
-            FirebaseDatabase.getInstance().reference.child("messages")
-                .push()
-                .setValue(
-                    Message(msg,email !! )
-                )
 
+            FirebaseDatabase.getInstance().reference.child("messages").push().setValue(
+                    Message(msg,email!! )
+                )
         }
+
         val query:DatabaseReference =  FirebaseDatabase.getInstance().reference.child("messages")
 
         val options: FirebaseListOptions<Message> = FirebaseListOptions.Builder<Message>()
             .setLayout(android.R.layout.simple_expandable_list_item_1)
-            .setQuery(query, Message::class.java)
-            .build()
+            .setQuery(query, Message::class.java).build()
 
-        val adapter: FirebaseListAdapter<Message> = object : FirebaseListAdapter<Message?>(options) {
-            override protected fun populateView(
-                v: View,
-                model: Message?,
-                position: Int
-            ) {
-                // Bind the Chat to the view
-                // ...
-                (v as TextView).text = model.email + "\n" + model.msg
+        val adapter: FirebaseListAdapter<Message> = object : FirebaseListAdapter<Message>(options) {
+                override fun populateView(v: View, model: Message, position: Int) {
+
+                    (v as TextView).text = model.email + "\n" + model.msg
+                }
             }
-        }
         adapter.startListening()
         messages.adapter = adapter
     }
 }
 class Message(var email: String, var msg: String) {
-    constructor() : this(email:"", msg:"")
+    constructor() : this (email:"", msg:"")
 }
